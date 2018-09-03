74f4f1f3d3715f127a12c9fd7543610a80fc8a32
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-09-03 01:24:46 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,5 +1,5 @@
 CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author_id INTEGER, series_id INTEGER);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER, characters_id INTEGER, books_id INTEGER);
-CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, sub_genre INTEGER);
+CREATE TABLE series (id INTEGER PRIMARY KEY, title TEXT, author_id INTEGER, subgenres_id INTEGER);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
-CREATE TABLE sub_genre (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE subgenres (id INTEGER PRIMARY KEY, name TEXT);
