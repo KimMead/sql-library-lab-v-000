1270c56101ac67ebf45d845da67df4494cd1186b
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-09-03 02:38:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,8 +24,7 @@ def select_series_title_with_most_human_characters
   "SELECT title FROM series
   JOIN characters ON characters.author_id = series.author_id
   GROUP BY species
-  WHERE species = 'human'
-  ORDER BY COUNT(*) DESC
+  ORDER BY COUNT(*) 
   LIMIT 1"
 end
 
