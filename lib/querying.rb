6c977fde696fe1903af1687b5d31a15a23323f5e
Kimberly Ann Meadows <kimmeadows8@gmail.com>
2018-09-03 02:31:42 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,11 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "Write your SQL query here"
+  "SELECT title, COUNT(*) FROM series
+  GROUP BY title
+  ORDER BY COUNT(*) DESC
+  LIMIT 1
+  WHERE species = 'human'"
 end
 
 def select_character_names_and_number_of_books_they_are_in
